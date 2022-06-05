13401ebc15fdcaa7103b668f392096ab39390ea4
rockiir <70121094+rockiir@users.noreply.github.com>
2022-06-05 00:29:10 -0300

    App Link GITHUB

--- ProjetoDecola/src/App.js
+++ ProjetoDecola/src/App.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { Text, View, StyleSheet, StatusBar, SafeAreaView } from 'react-native';
+import { 
+    Text, 
+    View, 
+    Image, 
+    StyleSheet, 
+    StatusBar, 
+    SafeAreaView,
+    Pressable,
+    Linking,
+ } from 'react-native';
 
+const colorGitHub = '#010409';
+const colorFontGitHub = '#C9D1D9';
+const colorDarkFontGitHub = '#616161';
+
+const imageProfileGitHub = 'https://avatars.githubusercontent.com/u/70121094?v=4';
+const urlMyGitHub = 'https://github.com/rockiir';
 const App = () => {
+    const handlePressGoToGithub = async () => {
+        console.log('verificando');
+        const res = await Linking.canOpenURL(urlMyGitHub)
+        console.log('aprovado');
+
+        if(res){
+            await Linking.openURL(urlMyGitHub)
+        }
+    }
     return (
         <SafeAreaView style={styles.container}>
-            <StatusBar backgroundColor={'black'} barStyle="light-content" />
-            <View style={styles.container}>
-                <Text style={styles.text}>
-                    Hello World!🎉
-                </Text>
+            <StatusBar backgroundColor={colorGitHub} barStyle="light-content" /*Barra de status*/ />
+            <View style={styles.content}>
+                <Image
+                    accessibilityLabel='avatar pessoa'
+                    style={styles.avatar}
+                    source={{ uri: imageProfileGitHub }} />
+                <Text accessibilityLabel="Nome Raquel matos"
+                style={styles.defaultText, styles.name}>Raquel matos</Text>
+                <Text accessibilityLabel='nickname' style={styles.defaultText, styles.nick}>@Rockiir</Text>
+                <Text accessibilityLabel='descrição' style={styles.defaultText, styles.description}> lorem ipsum | dolor sit amet| consectetur adipiscing elit </Text>
+            <Pressable onPress={handlePressGoToGithub}>
+            <View style={styles.button}>
+               <Text style={[styles.defaultText, styles.textButton]}> Open in Github</Text> 
+            </View>
+            </Pressable>
             </View>
         </SafeAreaView>
     );
@@ -18,13 +52,55 @@ export default App;
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'pink',
+        backgroundColor: colorGitHub,
         flex: 1, //Expande o background para toda a tela
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
 
     },
-    text: {
-        fontSize: 25,
+    defaultText: {
+        color: 'white',
+    },
+    name: {
+        marginTop: 20,
         fontWeight: 'bold',
-        color: 'red',
+
+        fontSize: 25,
+        color: colorFontGitHub,
+    },
+    nick: {
+        fontSize: 15,
+        color: colorDarkFontGitHub,
+    },
+    description: {
+        marginTop: 10,
+
+        fontSize: 14,
+        color: colorFontGitHub,
+    },
+    content: {
+        alignItems: 'center',
+        padding: 20,
+
+    },
+    avatar: {
+        height: 200,
+        width: 200,
+        borderRadius: 200,
+        borderColor: 'pink',
+        borderWidth: 2,
+
+    },
+    button: {
+        backgroundColor: colorDarkFontGitHub,
+        borderRadius: 10,
+        padding: 20,
+        marginTop: 50,
+
+    },
+    textButton: {
+        fontWeight: 'bold',
+        fontSize: 15,
     },
 });
--- README.md
+++ README.md
@@ -12,8 +12,78 @@ Executando simulação
 npm run android
 ```
 
+debugar no emulador+
 
-
-debugar no emulador
-
+```
 Ctrl + m
+```
+
+
+
+Instrodução ao react native
+
+##### Projeto GitHub
+
+<img src="file:///D:/gitRepo/Decola_Tech_react_native/imagens/gitapp.gif" title="" alt="" width="334">
+
+
+
+- Coinhecendo o react Native
+  
+  - O que é react native?
+  
+  - O que é cross-platform?
+  
+  - Origem do RN
+
+- Documentação:
+  
+  - Componentes 
+  
+  - API
+  
+  - Comunidade
+
+- Setup
+  
+  - Instalação Chocolatey
+  
+  - Node
+  
+  - Java 11 (JDK)
+  
+  - Yarn
+  
+  - Android Studio
+    
+    - download Androis e criando  AVD
+
+- Criação de projeto
+  
+  - Estrutura de pasta
+
+- Projeto Profile
+  
+  - View
+  
+  - Text
+  
+  - SafeArea
+  
+  - Image
+  
+  - Flex- box
+  
+  - style (styulesheet)
+  
+  - Array de estilo
+  
+  - Button
+  
+  - Pressable
+  
+  - Linking API
+  
+  - Add Queries no QAndroiManifest
+  
+  
