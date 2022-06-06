f60a28a9f2eae0013798cdc5cb5fbabb82e928fe
rockiir <70121094+rockiir@users.noreply.github.com>
2022-06-05 22:35:51 -0300

    projeto git

--- ProjetoDecola/src/App.js
+++ ProjetoDecola/src/App.js
@@ -13,6 +13,17 @@ import {
 const colorGitHub = '#010409';
 const colorFontGitHub = '#C9D1D9';
 const colorDarkFontGitHub = '#616161';
+const colorSquare = '#35d450';
+const Square = ({ text }) => (
+    <View style={styles.square}>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  );
+  const Square2 = ({ text }) => (
+    <View style={styles.square}>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  );
 
 const imageProfileGitHub = 'https://avatars.githubusercontent.com/u/70121094?v=4';
 const urlMyGitHub = 'https://github.com/rockiir';
@@ -38,6 +49,99 @@ const App = () => {
                 style={styles.defaultText, styles.name}>Raquel matos</Text>
                 <Text accessibilityLabel='nickname' style={styles.defaultText, styles.nick}>@Rockiir</Text>
                 <Text accessibilityLabel='descrição' style={styles.defaultText, styles.description}> lorem ipsum | dolor sit amet| consectetur adipiscing elit </Text>
+                <View style={styles.container2}>
+      <View style={styles.row}>
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+
+      </View>
+      <View style={styles.row}>
+      <Square  />
+      <View style={styles.square2} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      </View>
+      <View style={styles.row}>
+      <Square  />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      </View>
+      <View style={styles.row}>
+      <Square  />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      </View><View style={styles.row}>
+      <Square  />
+      <View style={styles.square2} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square2} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      </View>
+      <View style={styles.row}>
+      <Square  />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      <View style={styles.square} />
+      </View>
+      
+    </View>
             <Pressable onPress={handlePressGoToGithub}>
             <View style={styles.button}>
                <Text style={[styles.defaultText, styles.textButton]}> Open in Github</Text> 
@@ -52,12 +156,13 @@ export default App;
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: colorGitHub,
         flex: 1, //Expande o background para toda a tela
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
-
+        alignItems: "center",
+        backgroundColor: "black",
+        alignItems: "center",
     },
     defaultText: {
         color: 'white',
@@ -75,7 +180,6 @@ const styles = StyleSheet.create({
     },
     description: {
         marginTop: 10,
-
         fontSize: 14,
         color: colorFontGitHub,
     },
@@ -96,11 +200,43 @@ const styles = StyleSheet.create({
         backgroundColor: colorDarkFontGitHub,
         borderRadius: 10,
         padding: 20,
-        marginTop: 50,
+        marginTop: 30,
 
     },
     textButton: {
         fontWeight: 'bold',
         fontSize: 15,
     },
+    container2: {
+      backgroundColor: "black",
+
+
+      },
+      row: {
+        flexDirection: "row",
+      },
+      square: {
+        backgroundColor: "#006e33",
+        borderRadius: 7,
+        borderWidth: 1,
+        width: 20,
+        height: 20,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      square2: {
+        backgroundColor: colorSquare,
+        borderRadius: 7,
+        borderWidth: 1,
+        width: 20,
+        height: 20,
+        justifyContent: "center",
+        alignItems: "center",
+
+      },
+      text: {
+        color: "#6f707b",
+        fontSize: 18,
+        fontWeight: "bold",
+      },
 });
